97df46808a15ecf9d9b5f7b4d4a8f8bd667e83ed
Matheus <matheus.almeidasil@outlook.com>
2022-04-04 06:38:37 -0300

    Read implemented
    In revision, still needs to comment and do the errors treatments

--- stack_main.c
+++ stack_main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 #include "stack_header.h"
+#define MAX_Strings 1000
+#define MAX_Lenght  100
 
 
 int main(void){
@@ -11,6 +15,7 @@ int main(void){
     short int num;
     ptStack = Initialize_Stack();
 
+    /*
     // Teste com o programa do sor...
     ptStack = Clear(ptStack, ptControl);
     ptStack = Push(ptStack, ptControl, 4);
@@ -20,10 +25,130 @@ int main(void){
     ptStack = Pop(ptStack, ptControl);
     ptStack = Push(ptStack, ptControl, ptControl->reg);
     Output(ptStack);
-
-    /*
-    Implementar as funções de leitura do arquivo, fechamento e execução do algoritmo.
     */
 
+    char lines[MAX_Strings][MAX_Lenght];
+    char instruction[6];
+    bool reg = false;
+    bool has_num = false;
+    int j = 0;
+    const char* filename = "teste.asm";
+
+    FILE* input_file = fopen(filename, "r");
+    if (!input_file)
+        exit(EXIT_FAILURE);
+
+    char *buffer = NULL;
+    size_t len = 0;
+    while (getline(&buffer, &len, input_file) != -1){
+        int i = 0;
+        while (i <= strlen(buffer) + 1)
+        {
+            if (buffer[0] == ';'){
+                break;
+            } 
+            else if (buffer[i] == ';' || buffer[i] == '\0'){
+                strncpy(lines[j], buffer, i);
+                lines[j][i+1] = '\0';
+                j++;
+                break;
+            }
+            else{
+                i++;
+            }
+        } 
+    }
+
+    fclose(input_file);
+    free(buffer);
+
+
+    for (int k = 0; k < j; k++){
+        for (int i = 0; i <= strlen(lines[k] + 1); i++){
+            if ((lines[k][i] >= 'A' && lines[k][i] <= 'Z') || (lines[k][i] >= 'a' && lines[k][i] <= 'z') || lines[k][i] == '\0'){
+                if (reg == false){
+                    instruction[i] = lines[k][i];
+                }
+            }
+            else if (lines[k][i] >= '0' && lines[k][i] <= '9'){
+                num = lines[k][i] - '0';
+                has_num = true;
+            }
+            else if (lines[k][i] == '$' && (lines[k][i+1] == 'R'|| lines[k][i+1] == 'r')){
+                reg = true;
+            }
+        }
+
+
+        if (strcmp(instruction, "CLEAR") == 0 || strcmp(instruction, "clear") == 0){
+            ptStack = Clear(ptStack, ptControl);
+            printf("executa CLEAR\n");
+        }
+
+        if ((strcmp(instruction, "PUSH") == 0 || strcmp(instruction, "push") == 0) && has_num == true){
+            printf("%d-o\n", num);
+            ptStack = Push(ptStack, ptControl, num);
+            printf("executa PUSH N\n");
+        }
+
+        if ((strcmp(instruction, "PUSH") == 0 || strcmp(instruction, "push") == 0) && reg == true){
+            ptStack = Push(ptStack, ptControl, ptControl->reg);
+            printf("executa PUSH $R\n");
+        }
+
+        if (strcmp(instruction, "POP") == 0 || strcmp(instruction, "pop") == 0){
+            ptStack = Pop(ptStack, ptControl);
+            printf("executa POP\n");
+        }
+
+        if (strcmp(instruction, "ADD") == 0 || strcmp(instruction, "add") == 0){
+            Add(ptStack, ptControl);
+            printf("executa ADD\n");
+        }
+
+        if (strcmp(instruction, "SUB") == 0 || strcmp(instruction, "sub") == 0){
+            printf("executa SUB\n");
+        }
+
+        if (strcmp(instruction, "MUL") == 0 || strcmp(instruction, "mul") == 0){
+            printf("executa MUL\n");
+        }
+
+        if (strcmp(instruction, "DIV") == 0 || strcmp(instruction, "div") == 0){
+            printf("executa DIV\n");
+        }
+
+        if (strcmp(instruction, "MOD") == 0 || strcmp(instruction, "mod") == 0){
+            printf("executa MOD\n");
+        }
+
+        if (strcmp(instruction, "NOT") == 0 || strcmp(instruction, "not") == 0){
+            printf("executa NOT\n");
+        }
+
+        if (strcmp(instruction, "OR") == 0 || strcmp(instruction, "or") == 0){
+            printf("executa OR\n");
+        }
+
+        if (strcmp(instruction, "AND") == 0 || strcmp(instruction, "and") == 0){
+            printf("executa AND\n");
+        }
+
+        if (strcmp(instruction, "MIR") == 0 || strcmp(instruction, "mir") == 0){
+            printf("executa MIR\n");
+        }
+
+        if (strcmp(instruction, "OUT") == 0 || strcmp(instruction, "out") == 0){
+            Output(ptStack);
+            printf("executa OUT\n");
+        }
+        
+        memset(instruction,0,sizeof(instruction));
+        has_num = false;
+        reg = false;
+    }
+    
+
+    exit(EXIT_SUCCESS);
     return 0;
 }
